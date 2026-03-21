708893278f7d863e8d8fd8627d82b6468e738880
Tomi-PF <tperrino@fi.uba.ar>
2026-03-20 22:36:42 -0300

    Utilización de la navbar y agregado de las secciones de la página

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+/* Importación de los componentes */
+import Header from "./components/Header";
+
+/* Importación de las secciones */
+import Inicio from "./pages/Inicio"
+import Alojamientos from "./pages/Alojamientos"
+import Reservas from "./pages/Reservas"
+import Iniciar_sesion from "./pages/Iniciar_sesion"
+import Registrarse from "./pages/Registrarse"
 
 export default function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <Router>
+            <Header/>
+            <Container>
+                <Routes>
+                    <Route path="/" to={Inicio}/>
+                    <Route path="/alojamientos" to={Alojamientos}/>
+                    <Route path="/reservas" to={Reservas}/>
+                    <Route path="/iniciar_sesion" to={Iniciar_sesion}/>
+                    <Route path="/registrarse" to={Registrarse}/>
+                </Routes>
+            </Container>
+        </Router>
+    )
 }
